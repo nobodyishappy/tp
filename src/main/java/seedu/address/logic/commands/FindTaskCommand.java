ac1dcfbdcfdb20aa2aeb45772222cc926dd7be74
Kaya3842 <haichiendhnn1@gmail.com>
2024-03-29 02:10:36 +0800

    Create findtask command
    Create classes for the findtask command.
    
    Update AddressBookParser to register findtask command.

---
+++ src/main/java/seedu/address/logic/commands/FindTaskCommand.java
@@ -0,0 +1,65 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.task.TaskNameContainsKeywordsPredicate;
+
+/**
+ * Finds and lists all tasks in storage whose names contain any of the argument keywords.
+ * Keyword matching is case-insensitive.
+ */
+public class FindTaskCommand extends Command {
+    public static final String COMMAND_WORD = "findtask";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose names contain any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + "2101 2103t";
+
+    private final TaskNameContainsKeywordsPredicate predicate;
+
+    public FindTaskCommand(TaskNameContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    /**
+     * Executes the command and returns the result message.
+     *
+     * @param model {@code Model} which the command should operate on.
+     * @return feedback message of the operation result for display
+     * @throws CommandException If an error occurs during command execution.
+     */
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        model.updateFilteredTaskList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_TASKS_LISTED_OVERVIEW, model.getFilteredTaskList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FindTaskCommand)) {
+            return false;
+        }
+
+        FindTaskCommand otherFindTaskCommand = (FindTaskCommand) other;
+        return predicate.equals(otherFindTaskCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+}
---
+++ src/main/java/seedu/address/model/task/TaskNameContainsKeywordsPredicate.java
@@ -0,0 +1,53 @@
+package seedu.address.model.task;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Tests that a {@code Task}'s {@code TaskName} matches any of the keywords given.
+ */
+public class TaskNameContainsKeywordsPredicate implements Predicate<Task> {
+    private final List<String> keywords;
+
+    public TaskNameContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    /**
+     * Evaluates this predicate on the given argument.
+     *
+     * @param task The input argument
+     * @return {@code true} if the input argument matches the predicate,
+     * otherwise {@code false}
+     */
+    @Override
+    public boolean test(Task task) {
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(task.getName().toString(), keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof TaskNameContainsKeywordsPredicate)) {
+            return false;
+        }
+
+        TaskNameContainsKeywordsPredicate otherTaskNameContainsKeywordsPredicate =
+                (TaskNameContainsKeywordsPredicate) other;
+        return keywords.equals(otherTaskNameContainsKeywordsPredicate.keywords);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keywords", keywords).toString();
+    }
+
+}
