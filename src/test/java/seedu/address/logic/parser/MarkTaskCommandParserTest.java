40326db4bde3f2a82e1eb166bb76d7c67856d552
Chong Chin Herng <chongahzong@gmail.com>
2024-04-04 23:32:10 +0800

    Write tests for parsing multiple indices

--- src/test/java/seedu/address/logic/parser/MarkTaskCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/MarkTaskCommandParserTest.java
@@ -4,6 +4,7 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,7 +23,7 @@ public class MarkTaskCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsMarkTaskCommand() {
-        assertParseSuccess(parser, "1", new MarkTaskCommand(new Index[] { INDEX_FIRST }));
+        assertParseSuccess(parser, "1 2", new MarkTaskCommand(new Index[] { INDEX_FIRST, INDEX_SECOND }));
     }
 
     @Test
